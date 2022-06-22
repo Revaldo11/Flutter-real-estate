93838eab8e73850078ec5561c0e6206f457f159f
Revaldo Putra <revaldoputra11@gmail.com>
2022-06-22 22:21:56 +0700

    initial

---
+++ android/app/src/main/kotlin/com/example/real_este_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.real_este_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgrounColor = Color(0xffF5F5F5);
+Color primary = Color(0xffFBA651);
+Color black = Color(0xff191410);
+Color gray1 = Color(0xff7B756F);
+Color gray2 = Color(0xffADA8A4);
+Color gray3 = Color(0xffDDDBDA);
+Color gray4 = Color(0xffF7F8F8);
+Color white = Color(0xffFFFFFF);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+TextStyle titleTextStyle = GoogleFonts.montserrat(
+  fontSize: 16,
+  fontWeight: bold,
+  color: black,
+);
+
+TextStyle descTextStyle = GoogleFonts.montserrat(
+  fontSize: 12,
+  fontWeight: regular,
+  color: gray2,
+);
+
+TextStyle primaryTextStyle = GoogleFonts.montserrat(
+  fontSize: 18,
+  fontWeight: semiBold,
+  color: black,
+);
